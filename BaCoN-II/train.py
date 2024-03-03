682c37ef40a94fc02f58060b3f631cdfd3ac45e5
Arnav Agarwal <arnava1304@gmail.com>
2024-03-03 19:25:49 +0000

    Revert "Better exception handling in GPU mode"
    This reverts commit 611ae1f9217689b25871aedceeefd54659b67b31.
    
    Revert

--- BaCoN-II/train.py
+++ BaCoN-II/train.py
@@ -495,7 +495,8 @@ def main():
     if FLAGS.GPU:
         device_name = tf.test.gpu_device_name()
         if device_name != '/device:GPU:0':
-            raise SystemError('GPU device not found')
+            #raise SystemError('GPU device not found')
+            print('GPU device not found ! Device: %s' %device_name)
         else: print('Found GPU at: {}'.format(device_name))
         model=make_model(     model_name=model_name,
                                 drop=drop, 
