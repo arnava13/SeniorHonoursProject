721465d709fd47bd4d223d3be73549912ba3fa6b
Arnav Agarwal <arnava1304@gmail.com>
2024-03-03 18:42:20 +0000

    Bug fix

--- BaCoN-II/train.py
+++ BaCoN-II/train.py
@@ -638,7 +638,7 @@ def main():
             tpu_resolver = tf.distribute.cluster_resolver.TPUClusterResolver()  # Automatically detects the TPU
             tf.config.experimental_connect_to_cluster(tpu_resolver)  # Connects to the TPU cluster
             tf.tpu.experimental.initialize_tpu_system(tpu_resolver)  # Initializes the TPU for use
-            strategy = tf.distribute.experimental.TPUStrategy(resolver)
+            strategy = tf.distribute.experimental.TPUStrategy(tpu_resolver)
             tpu_device = tpu_resolver.master()  # Retrieves the TPU device URI
             print('Found TPU at: {}'.format(tpu_device))
             model=make_model(     model_name=model_name,
