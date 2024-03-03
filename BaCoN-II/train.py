611ae1f9217689b25871aedceeefd54659b67b31
Arnav Agarwal <arnava1304@gmail.com>
2024-03-03 19:08:28 +0000

    Better exception handling in GPU mode

--- BaCoN-II/train.py
+++ BaCoN-II/train.py
@@ -493,8 +493,7 @@ def main():
     if FLAGS.GPU:
         device_name = tf.test.gpu_device_name()
         if device_name != '/device:GPU:0':
-            #raise SystemError('GPU device not found')
-            print('GPU device not found ! Device: %s' %device_name)
+            raise SystemError('GPU device not found')
         else: print('Found GPU at: {}'.format(device_name))
         model=make_model(     model_name=model_name,
                                 drop=drop, 
