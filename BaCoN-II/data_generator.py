ec481ea618f8ba10348ffcffe847557315b9cdc0
Arnav Agarwal <arnava1304@gmail.com>
2024-03-12 17:00:11 +0000

    bugfix

--- BaCoN-II/data_generator.py
+++ BaCoN-II/data_generator.py
@@ -641,7 +641,7 @@ def create_generators(FLAGS):
     # SPLIT TRAIN/VALIDATION /(TEST)
     val_index = tf.random.shuffle(all_index)[:int(tf.floor(val_size*n_samples))]
     train_index_temp = tf.sparse.to_dense(tf.sets.difference(all_index[None, :], val_index[None, :]))[0]
-    test_size_eff = FLAGS.test_size/(tf.shape(train_index_temp)[0]/n_samples)
+    test_size_eff = float(FLAGS.test_size/(tf.shape(train_index_temp)[0]/n_samples))
     test_index = tf.random.shuffle(train_index_temp)[:tf.cast(tf.floor(test_size_eff * tf.cast(tf.shape(train_index_temp)[0], tf.float32)), tf.int32)]
     train_index = tf.sparse.to_dense(tf.sets.difference(train_index_temp[None, :], test_index[None, :]))[0]
 
