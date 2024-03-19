f344fde8b00d884cc48c6218e5fe95976d4bbac8
Arnav Agarwal <arnava1304@gmail.com>
2024-03-19 18:49:40 +0000

    bugfix

--- BaCoN-II/data_generator.py
+++ BaCoN-II/data_generator.py
@@ -593,11 +593,11 @@ class DataGenerator(tf.compat.v2.keras.utils.Sequence):
                     t_st =  self.data_root + '/'+l+ '/'+ str(ID) + '.txt' 
                     fname_list.append(t_st)
                     ID_list.append(ID)  
-            ID_list = tf.convert_to_tensor(ID_list, dtype=tf.int32)
+            ID_list = tf.constant(ID_list, dtype=tf.int32)
         else:
-             fname_list = get_fname_list(self.c_0, self.c_1, list_IDs, self.data_root,  list_IDs_dict, dataset_balanced=self.dataset_balanced,)
-             ID_list = [int(fname.split('.')[0].split('/')[-2]+'/'+fname.split('.')[0].split('/')[-1]) for fname in fname_list]
-             ID_list = tf.convert_to_tensor(ID_list, dtype=tf.int32)
+            fname_list = get_fname_list(self.c_0, self.c_1, list_IDs, self.data_root,  list_IDs_dict, dataset_balanced=self.dataset_balanced,)
+            ID_list = [int(fname.split('.')[0].split('/')[-2]+'/'+fname.split('.')[0].split('/')[-1]) for fname in fname_list]
+            ID_list = tf.constant(ID_list, dtype=tf.int32)
         if self.fine_tune and self.Verbose :
             tf.print(fname_list)
             
