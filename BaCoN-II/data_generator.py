6d31416ae1e41c32c0a91b242bf3cb39bbb59a8d
Arnav Agarwal <arnava1304@gmail.com>
2024-03-19 19:09:37 +0000

    bugfix

--- BaCoN-II/data_generator.py
+++ BaCoN-II/data_generator.py
@@ -365,17 +365,17 @@ class DataGenerator(tf.compat.v2.keras.utils.Sequence):
                     tf.print('Loading file %s' %fname)
         loaded_all = self.read_file(fname, dtype=tf.float32)
         def loop_over_noise(i_noise):
-            if self.add_noise:
-                            P_noisy = P_original
-                            if self.Verbose:
-                                tf.print('Noise realization %s' %i_noise.numpy())
-                            # add noise if selected
-                            if self.add_cosvar:
-                                noise_scale = generate_noise(k, P_noise, self.pi, sys_scaled=self.sys_scaled,
-                                                                sys_factor=self.sys_factor,sys_max=self.sys_max,
-                                                                add_cosvar=True, add_sys=False, add_shot=False, sigma_sys=self.sigma_sys)
-                                noise_cosVar = self.rng.normal(shape=noise_scale.shape, mean=0, stddev=noise_scale)
-                                P_noisy = P_noisy + noise_cosVar
+            P_noisy = P_original
+            if self.Verbose:
+                tf.print('Noise realization %s' %i_noise.numpy())
+            # add noise if selected
+            if self.add_cosvar:
+                noise_scale = generate_noise(k, P_noise, self.pi, sys_scaled=self.sys_scaled,
+                                                sys_factor=self.sys_factor,sys_max=self.sys_max,
+                                                add_cosvar=True, add_sys=False, add_shot=False, sigma_sys=self.sigma_sys)
+                noise_cosVar = self.rng.normal(shape=noise_scale.shape, mean=0, stddev=noise_scale)
+                P_noisy = P_noisy + noise_cosVar
+            return 0
         if self.TPU:
             with self.strategy.scope(): 
                 P_original = loaded_all[:, 1:]
@@ -385,7 +385,8 @@ class DataGenerator(tf.compat.v2.keras.utils.Sequence):
                     k = k[::self.sample_pace]
                 P_original, k = P_original[self.i_min:self.i_max], k[self.i_min:self.i_max]
                 self.k_range = tf.convert_to_tensor(k, dtype=tf.float32)
-                tf.map_fn(loop_over_noise, tf.range(self.n_noisy_samples))
+                if self.add_noise:
+                    tf.map_fn(loop_over_noise, tf.range(self.n_noisy_samples))
                     
         else:
             P_original = loaded_all[:, 1:]
@@ -404,7 +405,8 @@ class DataGenerator(tf.compat.v2.keras.utils.Sequence):
                 tf.print(P_original[10].numpy())
 
             P_noise = tf.gather(self.norm_data, self.z_bins, axis=1)
-            tf.map_fn(loop_over_noise, tf.range(self.n_noisy_samples))
+            if self.add_noise:
+                tf.map_fn(loop_over_noise, tf.range(self.n_noisy_samples))
 
         if self.add_noise and self.add_sys:
             curves_loaded = self.read_file(self.curve_file, dtype=tf.float32)
