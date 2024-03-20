1a5770c25d85cabf8bca5d95ab7cc7fc2f382e08
Arnav Agarwal <arnava1304@gmail.com>
2024-03-20 18:31:16 +0000

    bugfix

--- BaCoN-II/data_generator.py
+++ BaCoN-II/data_generator.py
@@ -366,9 +366,10 @@ class DataGenerator(tf.compat.v2.keras.utils.Sequence):
         'I dont know what exactly I should put here - where is n_channels ??? '
         return((len(self.list_IDs), self.dim[0]/self.sample_pace, self.dim[1] ))
     
+    @tf.function
     def load_sys(self, i_noise):
         curve_random_nr = self.rng.uniform(shape=[], minval=1, maxval=1001, dtype=tf.int32)
-        curve_random_nr = str(curve_random_nr.numpy())
+        curve_random_nr = tf.strings.as_string(curve_random_nr)
         curve_file = tf.io.gfile.join(self.curves_folder, '{}.txt'.format(curve_random_nr))
         curve_dat = self.read_file(curve_file, dtype=tf.float32)
         curve_dat = tf.cast(curve_dat, dtype=tf.float32)
