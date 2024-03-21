5bb6712c5d78071210b2253f3e4784e3e1f1f0a8
Arnav Agarwal <arnava1304@gmail.com>
2024-03-21 00:45:49 +0000

    bugfix

--- BaCoN-II/data_generator.py
+++ BaCoN-II/data_generator.py
@@ -654,8 +654,8 @@ class DataGenerator(tf.compat.v2.keras.utils.Sequence):
             dataset = dataset.batch(global_batchsize)
             dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
-        self.xshape = ((self.batch_size * self.n_batches).numpy(),) + tuple(self.x_shape_file)
-        self.yshape = ((self.batch_size * self.n_batches).numpy(),) + tuple(self.y_shape_file)
+        self.xshape = ((self.batch_size * self.n_batches).numpy(),) + tuple(self.xshape_file)
+        self.yshape = ((self.batch_size * self.n_batches).numpy(),) + tuple(self.yshape_file)
 
         
         return dataset
