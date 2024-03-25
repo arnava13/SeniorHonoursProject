e9c97076be11908e31eb13d287f20174fa518beb
Arnav Agarwal <arnava1304@gmail.com>
2024-03-25 22:10:10 +0000

    bugfix

--- BaCoN-II/data_generator.py
+++ BaCoN-II/data_generator.py
@@ -465,7 +465,7 @@ class DataSet():
     @tf.function
     def transformations(self, dataset):
         dataset = dataset.map(self.noise_realisations, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        dataset = dataset.flat_map(lambda x: tf.data.Dataset.from_tensor_slices(x))
+        dataset = dataset.flat_map(lambda x,y: tf.data.Dataset.from_tensor_slices((x,y)))
         if self.shuffle:
             dataset = dataset.shuffle(buffer_size=self.batch_size*self.n_batches)
         if self.TPU:
