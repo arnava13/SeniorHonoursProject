be8ce263421dd3bac5f47b9bd3e850ba71e6a806
Arnav Agarwal <arnava1304@gmail.com>
2024-03-27 00:30:01 +0000

    Remove model.fit training file and ensure ckpt and manager creation done with CPU in TPU mode.

--- BaCoN-II/train.py
+++ BaCoN-II/train.py
@@ -634,7 +634,11 @@ def main():
     else:
         ckpts_path=out_path+'/tf_ckpts_fine_tuning'+ft_ckpt_name_base_unfreezing+'/'
     ckpt_name = 'ckpt'
-    ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=model) 
+    if FLAGS.TPU:
+        with cpu_strategy.scope():
+            ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=model)
+    else:
+        ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=model)
     
     if FLAGS.fine_tune:
         print('Loading ckpt from %s' %ckpts_path)
@@ -693,8 +697,12 @@ def main():
             model(x)
         model.build(input_shape=input_shape)
         print(model.summary())
-    
-        ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=model)     
+
+        if FLAGS.TPU:
+            with cpu_strategy.scope():
+                ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=model)
+        else:
+            ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=model)     
     elif FLAGS.one_vs_all:
         if not FLAGS.test_mode:
             ckpts_path = out_path+'/tf_ckpts'+add_ckpt_name+'/'
@@ -704,9 +712,14 @@ def main():
         if FLAGS.test_mode:
             ckpt_name+='_test'
         
-        
-    manager = tf.train.CheckpointManager(ckpt, ckpts_path, 
+    if FLAGS.TPU:
+        with cpu_strategy.scope():
+            manager = tf.train.CheckpointManager(ckpt, ckpts_path, 
                                          max_to_keep=2, 
+                                         checkpoint_name=ckpt_name)
+    else:
+        manager = tf.train.CheckpointManager(ckpt, ckpts_path, 
+                                            max_to_keep=2, 
                                          checkpoint_name=ckpt_name)
     
     if FLAGS.TPU:
