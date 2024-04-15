bafae50a6ae047d08b6f00ead7b923101a117762
Arnav Agarwal <arnava1304@gmail.com>
2024-04-15 16:59:40 +0100

    Update params

--- BaCoN-II(i)/train-parameters.py
+++ BaCoN-II(i)/train-parameters.py
@@ -21,152 +21,153 @@ def handle_output(stream, log_file):
 def main():
    # Training options:
 
-   # ------------------ change the training directory and model name here ------------------
+    # ------------------ change the training directory and model name here ------------------
 
-   # training data directory path
-   DIR='data/train'
-   ## (batch size, is a multiple of #classes * noise realisations, e.g. 5 classes, 10 noise --> must be multiple of 50)
-   batch_size='12000'
+    # training data directory path
+    DIR='data/train'
+    ## (batch size, is a multiple of #classes * noise realisations, e.g. 5 classes, 10 noise --> must be multiple of 50)
+    batch_size='10000'
 
-   # training the network
-   GPU='True' # Use GPUs in training?
-   TPU='False' # Train with TPUs?
-   # which kind of model
-   bayesian='True' # Bayesian NN or traditional NN (weights single valued or distributions?)
-   n_epochs='100' # How many epochs to train for?
-
-
-   # ------------------ other parameters ------------------
-
-   # Directories
-   mypath=None # Parent directory
-
-   # Edit this with base model directory
-   mdir='models/'
-
-   # normalisation file
-   norm_data_name = '/planck_ee2.txt'
-
-   # scale cut and resolution
-   k_max='2.5' # max of k-modes to include?
-   k_min='0.0' # min of k-modes to include?
-   sample_pace='1'
-
-   # should the processed curves be saved in an extra file (False recommended, only efficient for n_epochs = 1)
-   save_processed_spectra='False'
-
-   # fine-tune = only 'LCDM' and 'non-LCDM'
-   fine_tune='False' # Model trained to distinguish between 'LCDM' and 'non-LCDM' classes or between all clases. i.e. binary or multi-classifier
-
-   # Cache directory, set as False to cache on memory.
-   cache_dir='False'
-
-   # -------------------- noise model --------------------
-
-   # noise model - Cosmic variance and shot noise are Euclid-like
-   n_noisy_samples='10' # How many noise examples to train on?
-   add_noise='True'# Add noise?
-   add_shot='False'# Add shot noise term? -- default should be False
-   add_sys='True'# Add systematic error term?
-   add_cosvar='True'# Add cosmic variance term?
-
-   # path to folder with theory error curves
-   curves_folder = 'data/curve_files_sys/theory_error'; sigma_curves_default = '0.05'
-   # change rescale distribution of theory error curves, uniform recommended
-   rescale_curves = 'uniform' # or gaussian or None
-
-   # sys error curves - relative scale of curve-amplitude
-   sigma_curves='0.05'
-
-   # ----------------- Model Name Generation -------------
-
-   #How many classes?
-   classnum = 'fivelabel'
-
-   #Examples Per Class?
-   examples_per_class = '20k'
-
-   #LCDM * Filter or Equal Examples from each class * Filter for randoms?
-   randstype = "equalexamples-randoms"
-   #randstype = "LCDM-randoms"
-
-   #Additional Notes?
-   fname_notes = ""
-
-   #Assemble fname
-   fname = f"{classnum}_{examples_per_class}_{randstype}_kmin-{k_min}_kmax-{k_max}_{fname_notes}"
-
-   # -------------------- additional training settings --------------------
+    # training the network
+    GPU='True' # Use GPUs in training?
+    TPU='False' # Train with TPUs?
+    # which kind of model
+    bayesian='True' # Bayesian NN or traditional NN (weights single valued or distributions?)
+    n_epochs='100' # How many epochs to train for?
 
 
-   # Type of model
-   model_name='custom' # Custom or dummy - dummy has presets as given in models.py
+    # ------------------ other parameters ------------------
 
-   #Test mode?
-   test_mode='False' # Network trained on 1 batch of minimal size or not?
-   seed='1312' # Initial seed for test mode batch
-   # Saving or restoring training
-   restore='False' # Restore training from a checkpoint?
-   save_ckpt='True' # Save checkpoints?
+    # Directories
+    mypath=None # Parent directory
+
+    # Edit this with base model directory
+    mdir='models/'
+
+    # normalisation file
+    norm_data_name = '/planck_ee2.txt'
+
+    # scale cut and resolution
+    k_max='2.5' # max of k-modes to include?
+    k_min='0.0' # min of k-modes to include?
+    sample_pace='1'
+
+    # should the processed curves be saved in an extra file (False recommended, only efficient for n_epochs = 1)
+    save_processed_spectra='False'
+
+    # fine-tune = only 'LCDM' and 'non-LCDM'
+    fine_tune='False' # Model trained to distinguish between 'LCDM' and 'non-LCDM' classes or between all clases. i.e. binary or multi-classifier
+
+    # Cache directory, set as False to cache on memory.
+    cache_dir='False'
+
+    # -------------------- noise model --------------------
+
+    # noise model - Cosmic variance and shot noise are Euclid-like
+    n_noisy_samples='10' # How many noise examples to train on?
+    add_noise='True'# Add noise?
+    add_shot='False'# Add shot noise term? -- default should be False
+    add_sys='True'# Add systematic error term?
+    add_cosvar='True'# Add cosmic variance term?
+
+    # path to folder with theory error curves
+    curves_folder = 'data/curve_files_sys/theory_error'; sigma_curves_default = '0.05'
+    # change rescale distribution of theory error curves, uniform recommended
+    rescale_curves = 'uniform' # or gaussian or None
+
+    # sys error curves - relative scale of curve-amplitude
+    sigma_curves='0.05'
+
+    # ----------------- Model Name Generation -------------
+
+    #How many classes?
+    classnum = 'fivelabel'
+
+    #Examples Per Class?
+    examples_per_class = '20k'
+
+    #LCDM * Filter or Equal Examples from each class * Filter for randoms?
+    randstype = "equalexamples-randoms"
+    #randstype = "LCDM-randoms"
+
+    #Additional Notes?
+    fname_notes = ""
+
+    #Assemble fname
+    fname = f"{classnum}_{examples_per_class}_{randstype}_kmin-{k_min}_kmax-{k_max}_{fname_notes}"
+
+    # -------------------- additional training settings --------------------
 
 
-   val_size='0.15' # Validation set % of data set
+    # Type of model
+    model_name='custom' # Custom or dummy - dummy has presets as given in models.py
 
-   add_FT_dense='False' #if True, adds an additional dense layer before the final 2D one
+    #Test mode?
+    test_mode='False' # Network trained on 1 batch of minimal size or not?
+    seed='1312' # Initial seed for test mode batch
+    # Saving or restoring training
+    restore='False' # Restore training from a checkpoint?
+    save_ckpt='True' # Save checkpoints?
 
-   patience='20' # terminate training after 'patience' epochs if no decrease in loss function
-   lr='0.01' # learning rate
-   decay='0.95' #decay rate: If None : Adam(lr),
 
-   padding='valid'
+    val_size='0.15' # Validation set % of data set
 
-   # -------------------- BNN parameters -----------------
+    add_FT_dense='False' #if True, adds an additional dense layer before the final 2D one
 
-   # Example image details
-   im_depth='500' # Number in z direction (e.g. 500 wave modes for P(k) examples)
-   im_width='1' # Number in y direction (1 is a 2D image : P(k,mu))
-   im_channels='4'  # Number in x direction (e.g. 4 redshifts for P(k,z))
-   swap_axes='True' # Do we swap depth and width axes? True if we have 1D image in 4 channels
-   sort_labels='True' # Sorts labels in ascending/alphabetical order
+    patience='20' # terminate training after 'patience' epochs if no decrease in loss function
+    lr='0.01' # learning rate
+    decay='0.95' #decay rate: If None : Adam(lr),
 
-   z1='0' # which z-bins to include? Assumes each channel is a new z bin.
-   z2='1' # which z-bins to include? Assumes each channel is a new z bin.
-   z3='2' # which z-bins to include? Assumes each channel is a new z bin.
-   z4='3' # which z-bins to include? Assumes each channel is a new z bin.
+    padding='valid'
 
-   # Number of layers and kernel sizes
-   k1='10'
-   k2='5'
-   k3='2'
-   # The dimensionality of the output space (i.e. the number of filters in the convolution)
-   f1='8'
-   f2='16'
-   f3='32'
-   # Stride of each layer's kernel
-   s1='2'
-   s2='2'
-   s3='1'
-   # Pooling layer sizes
-   p1='2'
-   p2='2'
-   p3='0'
-   # Strides in Pooling layer
-   sp1='2'
-   sp2='1'
-   sp3='0'
+    # -------------------- BNN parameters -----------------
 
-   n_dense='1' # Number of dense layers
+    # Example image details
+    im_depth='500' # Number in z direction (e.g. 500 wave modes for P(k) examples)
+    im_width='1' # Number in y direction (1 is a 2D image : P(k,mu))
+    im_channels='4'  # Number in x direction (e.g. 4 redshifts for P(k,z))
+    swap_axes='True' # Do we swap depth and width axes? True if we have 1D image in 4 channels
+    sort_labels='True' # Sorts labels in ascending/alphabetical order
 
-   # labels of different cosmologies
-   #c0_label = 'lcdm'
-   #c1_label = 'fR dgp wcdm'
+    z1='0' # which z-bins to include? Assumes each channel is a new z bin.
+    z2='1' # which z-bins to include? Assumes each channel is a new z bin.
+    z3='2' # which z-bins to include? Assumes each channel is a new z bin.
+    z4='3' # which z-bins to include? Assumes each channel is a new z bin.
 
-   log_path = mdir + fname + '_log'
+    # Number of layers and kernel sizes
+    k1='10'
+    k2='5'
+    k3='2'
+    # The dimensionality of the output space (i.e. the number of filters in the convolution)
+    f1='8'
+    f2='16'
+    f3='32'
+    # Stride of each layer's kernel
+    s1='2'
+    s2='2'
+    s3='1'
+    # Pooling layer sizes
+    p1='2'
+    p2='2'
+    p3='0'
+    # Strides in Pooling layer
+    sp1='2'
+    sp2='1'
+    sp3='0'
 
-   if fine_tune == "True":
-      log_path += '_FT'
+    n_dense='1' # Number of dense layers
 
-   log_path += '.txt'
+    # labels of different cosmologies
+    #c0_label = 'lcdm'
+    #c1_label = 'fR dgp wcdm'
+
+    log_path = mdir + fname + '_log'
+
+    if fine_tune == "True":
+        log_path += '_FT'
+
+    log_path += '.txt'
+
    # -------------- adapt c_0 and c_1 =  'fR', 'dgp', 'ds', 'rand' <<<<<<<<<<<<<<<
 
    # -------------- training loads parameters entered above  --------------
